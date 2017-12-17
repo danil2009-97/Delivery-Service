9014c2932b64917638c7940c76eab76c42035ef0
Даниил Попов <dvpopov_3@edu.hse.ru>
2017-12-17 22:21:45 +0300

    DB fixed

--- DeliveryServiceLogic/Migrations/201712171916223_Initial.Designer.cs
+++ DeliveryServiceLogic/Migrations/201712171916223_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace DeliveryServiceLogic.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class UserId : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(UserId));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201712171652493_UserId"; }
+            get { return "201712171916223_Initial"; }
         }
         
         string IMigrationMetadata.Source
--- DeliveryServiceUI/Pages/OrderPage.xaml.cs
+++ DeliveryServiceUI/Pages/OrderPage.xaml.cs
@@ -34,20 +34,5 @@ namespace DeliveryServiceUI
             ordersListBox.ItemsSource = Factory.Default.GetRepositoryCRUD<Order>().Data.FindAll(o => o.UserId == Factory.Default.LoggedUser.Id).OrderByDescending(o => o.OrderedTime);
         }
 
-        private void ordersListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if (ordersListBox.SelectedIndex != -1)
-            {
-                var ord = ordersListBox.SelectedItem as Order;
-                var res = MessageBox.Show($"Удалить заказ?", "Вы уверены?", MessageBoxButton.YesNo, MessageBoxImage.Question);
-
-                if (res == MessageBoxResult.Yes)
-                {
-                    Factory.Default.GetRepositoryCRUD<Order>().RemoveItem(ord);
-                    RefreshListBox();
-                }
-                else { return; }
-            }
-        }
     }
 }
